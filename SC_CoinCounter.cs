86319bac8847f838c240eca746ef87f69db5e3ef
MartynasRimk <126596170+MartynasRimk@users.noreply.github.com>
2023-04-19 00:33:05 +0300

    Added scripts written by me

---
+++ SC_CoinCounter.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class SC_CoinCounter : MonoBehaviour
+{
+    private TextMeshProUGUI counterText;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        counterText = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Set the current number of coins to display
+        if (counterText.text != SC_2DCoin.totalCoins.ToString()+"/"+CollectCoinsToContinue.maxCoins)
+        {
+            counterText.text = SC_2DCoin.totalCoins.ToString()+"/"+CollectCoinsToContinue.maxCoins;
+        }
+    }
+}
