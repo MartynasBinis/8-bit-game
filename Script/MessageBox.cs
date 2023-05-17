9e16f0354f0ea5ee93d9e0331d8dcab670f420c5
Matauskas <104345266+Matauskas@users.noreply.github.com>
2023-05-17 13:34:27 +0300

    Add files via upload

---
+++ Script/MessageBox.cs
@@ -0,0 +1,25 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MessageBox : MonoBehaviour
+{
+    public GameObject Message;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        if (collision.gameObject.tag == "Player")
+        {
+            Message.SetActive(true);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.tag == "Player")
+        {
+            Message.SetActive(false);
+        }
+    }
+}
